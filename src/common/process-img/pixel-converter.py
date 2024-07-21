1669c70a9f9fd351a681dac6d5f6a84fece956b0
quanghuy6698 <quanghuy6698@gmail.com>
2024-07-22 00:04:01 +0700

    update code

--- src/common/process-img/pixel-converter.py
+++ src/common/process-img/pixel-converter.py
@@ -2,13 +2,13 @@ import cv2
 import numpy as np
 
 img_file_path = "E:/thac-si-cntt-TLU/final-project/img2music/asset/demo/leaves3.png"
+out_file_path = "E:/thac-si-cntt-TLU/final-project/img2music/asset/demo/leaves3.txt"
 
 image = cv2.imread(img_file_path)
 img_h, img_w = image.shape[:2]
 image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 pixel_array = np.array(image_rgb)
 
-out_file_path = "E:/thac-si-cntt-TLU/final-project/img2music/asset/demo/leaves3.txt"
 f = open(out_file_path, "a")
 for i in pixel_array:
     for j in i:
--- src/common/util/txt-file-handler.py
+++
@@ -1,8 +0,0 @@
-def read_file(file_path):
-    f = open(file_path, "r")
-    return f.read()
-
-def write_to_file(file_path, content):
-    f = open(file_path, "w")
-    f.write(content)
-    f.close()
